fb97ff7108a37ab65838620c8808518b7834e3d7
Tomas Drencak <tomas@tomas.local>
2016-02-04 21:58:48 +0100

    react app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-(function (window) {
+(function (window, reframe, React, ReactDOM) {
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
 
-})(window);
+	var TodoApp = reframe.view('TodoApp', function () {
+		return React.DOM.section(
+			{className: 'todoapp'},
+			React.DOM.header(
+				{className: 'header'},
+				React.DOM.h1(null, 'todos'),
+				React.DOM.input({
+					className: 'new-todo',
+					placeholder: 'What needs to be done?',
+					autofocus: true })
+			)
+		);
+	});
+	ReactDOM.render(TodoApp(), window.document.getElementById("app"));
+})(window, reframe, React, ReactDOM);
