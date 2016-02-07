1469def4f5d7898547aeb658afc05dd5c951da89
Tomas Drencak <tomas@tomas.local>
2016-02-07 20:06:40 +0100

    1 item storage

--- js/storage.js
+++ js/storage.js
@@ -13,39 +13,37 @@ window.TodoAppStorage = (function (reframe, localStorage) {
 					return db.get('items');
 				})
 				.distinctUntilChanged(identity, Immutable.is)
-				.scan(function (acc, newItems) {
-					var ids = newItems.keySeq().toSet();
-					return {
-						current: newItems,
-						ids: ids,
-						remove: acc.ids.subtract(ids),
-						update: newItems.filter(function (item) {
-							return item !== acc.current.get(item.get('id'));
-						})
-					};
-				}, {
-					current: Immutable.OrderedMap(),
-					ids: Immutable.Set()
-				})
-				.subscribe(function (acc) {
-					localStorage.setItem('todos-reframejs', JSON.stringify(acc.ids.toJS()));
-
-					acc.remove.forEach(function (id) {
-						localStorage.removeItem('todos-reframejs-' + id);
-					});
-					acc.update.forEach(function (item) {
-						localStorage.setItem('todos-reframejs-' + item.get('id'), JSON.stringify(item.toJS()));
-					});
+				.subscribe(function(items) {
+					localStorage.setItem('todos-reframejs', JSON.stringify(items.valueSeq().toJS()));
 				});
+				//.scan(function (acc, newItems) {
+				//	var ids = newItems.keySeq().toSet();
+				//	return {
+				//		current: newItems,
+				//		ids: ids,
+				//		remove: acc.ids.subtract(ids),
+				//		update: newItems.filter(function (item) {
+				//			return item !== acc.current.get(item.get('id'));
+				//		})
+				//	};
+				//}, {
+				//	current: Immutable.OrderedMap(),
+				//	ids: Immutable.Set()
+				//})
+				//.subscribe(function (acc) {
+				//	localStorage.setItem('todos-reframejs', JSON.stringify(acc.ids.toJS()));
+                //
+				//	acc.remove.forEach(function (id) {
+				//		localStorage.removeItem('todos-reframejs-' + id);
+				//	});
+				//	acc.update.forEach(function (item) {
+				//		localStorage.setItem('todos-reframejs-' + item.get('id'), JSON.stringify(item.toJS()));
+				//	});
+				//});
 		},
 		loadState: function (initFilter) {
 			if (localStorage) {
-				var ids = JSON.parse(localStorage.getItem('todos-reframejs')) || [];
-				var items = ids
-					.map(function (id) {
-						return JSON.parse(localStorage.getItem('todos-reframejs-' + id));
-					})
-					.filter(Boolean);
+				var items = JSON.parse(localStorage.getItem('todos-reframejs')) || [];
 
 				reframe.dispatchSync(['resetDb', items, initFilter]);
 			} else {
